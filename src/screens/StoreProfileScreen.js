23c379862380747b4470b4eebd4f420924a4d495
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-06 02:36:14 +0500

    profile

--- src/screens/StoreProfileScreen.js
+++ src/screens/StoreProfileScreen.js
@@ -20,9 +20,7 @@ const StoreProfileScreen = ({navigation}) => {
   return (
     <SafeAreaView style={COMMON.super_Container}>
       <StoreProfileHeader title={'Tradly Store'} />
-      <View style={styles.container}>
-        <View style={styles.imageContainer}></View>
-      </View>
+      <View style={styles.container}></View>
     </SafeAreaView>
   );
 };
